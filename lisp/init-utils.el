8c4502cdc9aec366d7179da8e90fcbcd5d0224e9
nasoundead <nasoundead@163.com>
2024-07-14 21:06:35 +0800

    fanyi

--- lisp/init-utils.el
+++ lisp/init-utils.el
@@ -1,4 +1,15 @@
-
+(use-package fanyi
+  :ensure t
+  :custom
+  (fanyi-providers '(;; 海词
+		     ;; fanyi-haici-provider
+		     ;; 有道同义词词典
+		     fanyi-youdao-thesaurus-provider
+		     ;; Etymonline
+		     fanyi-etymon-provider
+		     ;; Longman
+		     ;; fanyi-longman-provider)
+		     )))
 
 
 
