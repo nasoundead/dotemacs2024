ba44ad57c39ca5355c03743c3dfdf5e9e39671cd
nasoundead <8845497+nasoundead@user.noreply.gitee.com>
2025-09-13 21:50:55 +0800

    no minuet

--- lisp/init-ai.el
+++ lisp/init-ai.el
@@ -73,35 +73,35 @@
 ;;       (unless (string-match "The free version of TabNine only indexes up to" (funcall company-message-func))
 ;;         ad-do-it))))
 
-(use-package minuet
-:straight (minuet
-	   :type git
-	   :host github
-	   :repo "emacsmirror/minuet")
- :bind
- (("M-y" . #'minuet-complete-with-minibuffer) ;; use minibuffer for completion
-  ("M-i" . #'minuet-show-suggestion) ;; use overlay for completion
-  ("C-c m" . #'minuet-configure-provider)
-  :map minuet-active-mode-map
-  ;; These keymaps activate only when a minuet suggestion is displayed in the current buffer
-  ("M-p" . #'minuet-previous-suggestion) ;; invoke completion or cycle to next completion
-  ("M-n" . #'minuet-next-suggestion) ;; invoke completion or cycle to previous completion
-  ("M-A" . #'minuet-accept-suggestion) ;; accept whole completion
-  ;; Accept the first line of completion, or N lines with a numeric-prefix:
-  ;; e.g. C-u 2 M-a will accepts 2 lines of completion.
-  ("M-a" . #'minuet-accept-suggestion-line)
-  ("M-e" . #'minuet-dismiss-suggestion))
+;; (use-package minuet
+;; :straight (minuet
+;;	   :type git
+;;	   :host github
+;;	   :repo "emacsmirror/minuet")
+;;  :bind
+;;  (("M-y" . #'minuet-complete-with-minibuffer) ;; use minibuffer for completion
+;;   ("M-i" . #'minuet-show-suggestion) ;; use overlay for completion
+;;   ("C-c m" . #'minuet-configure-provider)
+;;   :map minuet-active-mode-map
+;;   ;; These keymaps activate only when a minuet suggestion is displayed in the current buffer
+;;   ("M-p" . #'minuet-previous-suggestion) ;; invoke completion or cycle to next completion
+;;   ("M-n" . #'minuet-next-suggestion) ;; invoke completion or cycle to previous completion
+;;   ("M-A" . #'minuet-accept-suggestion) ;; accept whole completion
+;;   ;; Accept the first line of completion, or N lines with a numeric-prefix:
+;;   ;; e.g. C-u 2 M-a will accepts 2 lines of completion.
+;;   ("M-a" . #'minuet-accept-suggestion-line)
+;;   ("M-e" . #'minuet-dismiss-suggestion))
 
- :init
- ;; if you want to enable auto suggestion.
- ;; Note that you can manually invoke completions without enable minuet-auto-suggestion-mode
- (add-hook 'prog-mode-hook #'minuet-auto-suggestion-mode)
+;;  :init
+;;  ;; if you want to enable auto suggestion.
+;;  ;; Note that you can manually invoke completions without enable minuet-auto-suggestion-mode
+;;  (add-hook 'prog-mode-hook #'minuet-auto-suggestion-mode)
 
- :config
- ;; You can use M-x minuet-configure-provider to interactively configure provider and model
- (setq minuet-provider 'openai-fim-compatible)
+;;  :config
+;;  ;; You can use M-x minuet-configure-provider to interactively configure provider and model
+;;  (setq minuet-provider 'openai-fim-compatible)
 
- (minuet-set-optional-options minuet-openai-fim-compatible-options :max_tokens 64))
+;;  (minuet-set-optional-options minuet-openai-fim-compatible-options :max_tokens 64))
 
 ;; For Evil users: When defining `minuet-ative-mode-map` in insert
 ;; or normal states, the following one-liner is required.
